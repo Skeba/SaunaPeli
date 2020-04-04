5957e264a084c279b9cd106b0b8a7a1c5e343341
Pygee <toni.pylvalainen91@gmail.com>
2020-04-05 01:22:50 +0300

    Update PlayerController.cs
    Added Feature Jump to Character

--- SaunaPeli/Assets/Scripts/PlayerController.cs
+++ SaunaPeli/Assets/Scripts/PlayerController.cs
@@ -5,23 +5,20 @@ using UnityEngine;
 public class PlayerController : Main
 {
     // Start is called before the first frame update
-    protected override void Start()
-    {
+    protected override void Start() {
         base.Start();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         MovePlayer();
+        Jump();
     }
 
-    private void MovePlayer()
-    {
+    private void MovePlayer() {
         float horizontalMovement = Input.GetAxis("Horizontal");
 
-        if (Mathf.Abs(horizontalMovement) > Mathf.Epsilon)
-        {
+        if (Mathf.Abs(horizontalMovement) > Mathf.Epsilon) {
             horizontalMovement = horizontalMovement * Time.deltaTime * gameSceneController.playerSpeed;
             horizontalMovement += transform.position.x;
 
@@ -31,6 +28,12 @@ public class PlayerController : Main
             float limit = Mathf.Clamp(horizontalMovement, left, right);
 
             transform.position = new Vector2(limit, transform.position.y);
+        }
+    }
+
+    void Jump() {
+        if(Input.GetButtonDown("Jump")) {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
         }
     }
 }
