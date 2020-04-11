2fe7b74be160e89ce29eb071f0e8b70cd41132b0
Pygee <toni.pylvalainen91@gmail.com>
2020-04-11 18:38:44 +0300

    Water will destroy you

---
+++ SaunaPeli/Assets/Scripts/Destroy/DestroyPlayer.cs
@@ -0,0 +1,9 @@
+﻿ using UnityEngine;
+ 
+  public class DestroyPlayer : MonoBehaviour {
+
+void OnCollisionEnter2D (Collision2D col){
+     if(col.gameObject.tag == "Player")
+     Destroy(col.gameObject);
+     }
+ }
